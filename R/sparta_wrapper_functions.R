178895fa7fdf4728724355a9f5385ecd6d3d7aa3
Diana Bowler <diana.bowler@nina.no>
2018-02-01 10:21:51 +0100

    Diana updated the code so it also works for models with regional codes

--- R/sparta_wrapper_functions.R
+++ R/sparta_wrapper_functions.R
@@ -1,5 +1,10 @@
 #given the directory in which the sparta model outputs are found, put all the models in a list
+#@param dir = directory of sparta model outputs
+
 getSpartaModels<-function(dir){
+  
+  if(!dir%in%list.files()) stop('no such directory found')  
+  
   list.files(dir) -> sp_mods
   
   models <- lapply(sp_mods, function(sp) {
@@ -7,48 +12,74 @@ getSpartaModels<-function(dir){
     return(out)
   }
   )
+  
   names(models) <- gsub(sp_mods, pa="\\.rdata", repl="")
   
   return(models)
 }
 
 #get annual predictions of occupancy for each species
+#@param models = output of getSpartaModels()
+
 annualPredictions <- function(models){
-  library(sparta)
-  library(plyr)
-  ldply(models,function(x){
+ 
+  require(sparta)
+  require(plyr)
   
-  #get annual predictions
-  temp <- data.frame(summary(x))
-  temp$Year<-as.numeric(row.names(summary(x)))
-  temp$Species <- x$SPP_NAME
+  out<-ldply(models,function(x){
   
-  #get RHat values
-  bugsOutput <- x$BUGSoutput$summary
-  bugsOutput <- data.frame(bugsOutput[grepl("psi.fs",row.names(bugsOutput)),])
-  temp$Rhat <- as.numeric(bugsOutput$Rhat)
+  #get bugs output
+  temp <- x$BUGSoutput$summary
+  temp <- temp[grep("psi.fs\\[",row.names(temp)),]
+  temp <- data.frame(temp)
+  names(temp) <- gsub("X2.5.","quant_025", names(temp)) 
+  names(temp) <- gsub("X97.5.","quant_975", names(temp)) 
+
+  #add on year and species info
+  temp$Year <- x$min_year:x$max_year
+  temp$Species <- x$SPP_NAME  
+    
+  #get records per species
+  temp$nuRecords <- x$species_observations
+  temp$nuSites <- x$nsites
+  
+  #reorganise
+  temp<-temp[,c("Species","Year","mean","quant_025","quant_975","Rhat","n.eff","nuRecords","nuSites")]
   
   return(temp)
 })
-  
+
+  out<-out[,-1]#get rid of superfluous first column
+  return(out)
+
 }
 
 #plot these predictions (restrict to species with more than 50 observations)
-plotPredictions <- function(myAnnualPredictions,rawdata){
-  
+#@param myAnnualPredictions = output from annualPredictions()
+
+plotPredictions <- function(myAnnualPredictions){
+
+#decide on year breaks to nearest decade
+surveyYears<-sort(unique(myAnnualPredictions$Year))
+surveyYears<-surveyYears[surveyYears%%10==0]
+
 require(ggplot2)
-ggplot(data=subset(myAnnualPredictions,Species %in% names(table(rawdata$Species))[table(rawdata$Species)>50])) +
+  
+#plot
+ggplot(data=subset(myAnnualPredictions,nuRecords>50)) +
   geom_line(aes(x = Year, mean))+
   geom_point(aes(x = Year, mean,colour = factor(Rhat<1.1)))+
   geom_ribbon(aes(x=Year, ymin = quant_025, ymax = quant_975), alpha=0.50)+
   theme_bw() +
-  scale_x_continuous(labels=c(1990,1995,2000,2005,2010))+
+  scale_x_continuous(breaks = surveyYears, labels = surveyYears)+
   facet_wrap( ~ Species) +
-  theme(legend.position = "none")
+  theme(legend.position = "none") +
+  ylab("Predicted occupancy proportion")
 }
 
 
 #get estimates of each species linear population trends and 95% CI
+#@param models = output of getSpartaModels()
 calculateTrends<-function(models){
 
   #get trends for each model
