92450315b81994b5c7180bc402120036cff5e7f8
Diana Bowler <diana.bowler@nina.no>
2018-04-03 10:44:48 +0200

    Diana added a model that allows for missing date info.

---
+++ R/BUGS_sparta_missing_data.R
@@ -0,0 +1,85 @@
+cat("
+  model{
+  # JAGS code for SPARTA model plus random walk prior
+  # on the year effect of the state model + intercept + halfcauchy hyperpriors
+  
+  # State model
+  for (i in 1:nsite){ 
+    for (t in 1:nyear){
+      z[i,t] ~ dbern(muZ[i,t]) 
+      logit(muZ[i,t])<- a[t] + eta[i] # year as a fixed factor and site as a random factor
+    }
+  }   
+  
+  
+  ### Observation Model
+  for(j in 1:nvisit) {
+    y[j] ~ dbern(Py[j]) #data is Y
+    Py[j]<- z[site[j],year[j]]*p[j] #probability to detect = prob of occ * prob of detection
+
+    #detection model:
+    logit(p[j]) <-  alpha.p[year[j]] + beta1*Jul_date[j] + beta2*pow(Jul_date[j], 2) + dtype.p*L[j]
+    #depends on year, julian date and list length
+    } 
+  
+  #prior for the missing covariates - julian date
+  for(j in 1:nvisit){
+    Jul_date[j] ~ dnorm(juldat.mean,juldat.tau)
+  }
+
+  #vague priors
+  juldat.mean ~ dnorm(0,0.0001)
+  juldat.tau <- pow(juldat.sd,-2)
+  juldat.sd ~ dunif(0,100)
+  
+  #prior for the missing covariates - list length
+  for(j in 1:nvisit){
+    L[j] ~ dpois(l.mu[j])
+    log(l.mu[j]) <- l.mean 
+  }
+  l.mean ~  dnorm(0,0.0001)
+
+  # Derived parameters
+  for (t in 1:nyear) {  
+    psi.fs[t] <- sum(z[1:nsite, t])/nsite
+  } 
+
+  #Priors 
+
+  # State model priors
+    #year effects
+    #year 1
+    a[1] ~ dnorm(0, 0.001)
+
+    #other years
+    for(t in 2:nyear){
+      a[t] ~ dnorm(a[t-1], tau.a)
+    }
+    tau.a <- 1/(sd.a * sd.a)
+    sd.a ~ dt(0, 1, 1)T(0,) 
+    
+    #site effects
+    for (i in 1:nsite) {
+      eta[i] ~ dnorm(0, tau2)       
+    } 
+    tau2 <- 1/(sigma2 * sigma2) 
+    sigma2 ~ dt(0, 1, 1)T(0,) 
+
+
+    #Observation model priors 
+    #year effects
+    for (t in 1:nyear) {
+      alpha.p[t] ~ dnorm(mu.lp, tau.lp)            
+    }
+    
+    mu.lp ~ dnorm(0, 0.01)
+    tau.lp <- 1 / (sd.lp * sd.lp)                 
+    sd.lp ~ dt(0, 1, 1)T(0,)  
+    
+    #observation model covariates
+    beta1 ~ dnorm(0, 0.0001)
+    beta2 ~ dnorm(0, 0.0001)
+    dtype.p ~ dnorm(0, 0.01)
+    
+  }
+    ",fill=TRUE,file="BUGS_sparta_missing_data.txt")
