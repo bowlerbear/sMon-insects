fd10d6de79ad1a96d38ab2277095a03ae77e442e
Bowler <db40fysa@idiv.de>
2018-10-01 08:39:22 +0200

    Retreive coordinates

---
+++ environmentalData.R
@@ -0,0 +1,7 @@
+#get information on the MTBs
+setwd("C:/Users/db40fysa/Nextcloud/sMon-Analyses/MTB_Q Informations")
+
+library(gdata)
+coords <- read.xls("German_grid_central_coordinates.xls",sheet=5)
+
+
