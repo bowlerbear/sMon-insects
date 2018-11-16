797f5cda6ee6055ebea00846191ec2eb01b33ae7
Diana Bowler <db40fysa@rstudio1.idiv.de>
2018-11-16 13:45:51 +0100

    first trait and trend analysis

--- trait_trend_analysis.R
+++ trait_trend_analysis.R
@@ -5,20 +5,43 @@ source('C:/Users/db40fysa/Nextcloud/sMon-Analyses/sMon-insects/R/sparta_wrapper_
 #######################################################################
 
 #get annual indicies
-load("model-outputs/out_RpS_Expertise_trends.RData")
-out2 <- combineModels(out)
-outTrends <- getBUGSfits(out2,param="psi.fs") 
+
+trendFiles <- list.files("derived-data/")[grepl("outTrends",list.files("derived-data/"))]
+allTrends <- ldply(trendFiles, function(x){
+  load(paste("derived-data",x,sep="/"))
+  
+  #get file metadata
+  outTrends$stage <- strsplit(x,"_")[[1]][3]
+  outTrends$state <- strsplit(x,"_")[[1]][4]
+  outTrends$state <- strsplit(outTrends$state ,"\\.")[[1]][1]
+  
+  #return data frame
+  return(outTrends)
+})
 
 #######################################################################
 
 #compare adult and juvenile trends
 
+ggplot(subset(allTrends,state=="Sa"),
+       aes(x=ParamNu,y=mean,colour=stage))+
+  geom_line()+
+  facet_wrap(~Species)+
+  theme(legend.position="none")
+
+ggplot(subset(allTrends,state=="SH"),
+       aes(x=ParamNu,y=mean,colour=stage))+
+  geom_line()+
+  facet_wrap(~Species)+
+  theme(legend.position="none")
+
+
 #######################################################################
 
-fitTrends <- function(df,myspecies=NULL){
+fitTrends <- function(df){
 
 #get data for species
-  dfS <- subset(df,Species==myspecies)
+  dfS <- df
   dfS <- dfS[order(dfS$ParamNu),]
   
 #combine for a bugs model
@@ -67,15 +90,57 @@ inits <- function(){
 out1 <- jags(bugs.data, inits=inits, params, "R/Bugs_trend.txt", n.thin=nt,
              n.chains=3, n.burnin=1000,n.iter=5000,parallel=T)
 
+#return predictions
+return(data.frame(trend=out1$mean$trend,trend_sd=out1$sd$trend))
+
 }
+
+#####################################################################
+
+#fit to each stage and state
+
+trendEstimates <- ddply(allTrends,.(Species,state,stage),
+                        function(x){
+                          fitTrends(x)
+                          })
 
 ######################################################################
 
-
 #get traits data
-load("altraits.RData")
-#limit to those with complete cases
+load("alltraits.RData")
+#limit to those with complete cases?
 
+#########################################################################
+
+#plot of traits vs trends
+trendEstimate <- merge(trendEstimates,alltraits,by="Species")
+
+#temperature preference plot
+ggplot(trendEstimate,aes(x=TMean,y=trend))+
+  geom_point()+
+  facet_wrap(state~stage)
+
+ggplot(trendEstimate,aes(x=TMean,y=trend))+
+  geom_point(aes(colour=state,shape=stage))+
+  stat_smooth(method="gam")
+
+#habitat preference
+ggplot(trendEstimate,aes(x=Habitat.y,y=trend))+
+  geom_boxplot()+
+  facet_wrap(state~stage)
+
+#range size
+ggplot(trendEstimate,aes(x=nuEuroGrids,y=trend))+
+  geom_point()+
+  facet_wrap(state~stage)
+
+#generation time
+ggplot(trendEstimate,aes(x=development.time..mean.,y=trend))+
+  geom_point()+
+  facet_wrap(state~stage)
+
+ggplot(trendEstimate,aes(x=development.time..mean.,y=trend))+
+  geom_point(aes(colour=state,shape=stage))
 
 #####################################################################
 
@@ -99,7 +164,6 @@ cat("
                     year[t] + 
                     inprod(beta[],traitsDF[species[i],]) + 
                     year[t] * inprod(betaInt[],traitsDF[species[i],])      
-
 
    for(i in 1:n.covs){
       beta[i] ~ dnorm(0,0.1)
