edb94487d51e09743ff684b915b8a81460b34fe0
Bowler <db40fysa@idiv.de>
2019-04-11 09:50:43 +0200

    initial commit for power analysis

---
+++ powerAnalysis.R
@@ -0,0 +1,27 @@
+#powerAnalysis:
+
+#########################################################################################################
+#- test the ability of the predicted occupancy models to estimate trends of a given amount
+
+#for each species, how strong would the real trend have to be before we could detect it, given the SE of the occupancies
+
+#given this SE and mean occupancy, we could only detect a change in occupancy of X
+
+#get this for each species??
+
+###########################################################################################################
+
+#YearA, 80 (10) 
+#YearB, 100 (20)
+
+#given the SE, what sort of trend would be difficult from zero at 5%
+
+
+###########################################################################################################
+
+
+
+
+
+
+
