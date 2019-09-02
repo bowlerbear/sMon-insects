eedd6dbf2bea9d755c49bb4e4fe3c2c32086026e
Bowler <db40fysa@idiv.de>
2019-09-02 09:47:35 +0200

    state and national-level analysis

--- trait_trend_analysis.R
+++ trait_trend_analysis.R
@@ -4,23 +4,28 @@ source('C:/Users/db40fysa/Nextcloud/sMon-Analyses/Git/sMon-insects/R/sparta_wrap
 
 ######################################################################
 setwd("C:/Users/db40fysa/Nextcloud/sMon-Analyses/Git/sMon-insects")
+
 #get traits data
 load("alltraits.RData")
 #limit to those with complete cases?
 
+#get trends data
+#load("model-outputs/trendEstimatesNational.RData")
 load("model-outputs/trendEstimates.RData")
+
+trendEstimates$Species[!trendEstimates$Species %in% alltraits$Species]
+trendEstimates <- merge(trendEstimates,alltraits,by="Species")
 
 #########################################################################
 
-#plot of traits vs trends
-trendEstimates <- merge(trendEstimates,alltraits,by="Species")
+#lm of traits vs trends
 
 #temperature preference plot
 ggplot(trendEstimates,aes(x=TMean,y=trend))+
   geom_point()+
   facet_wrap(State~Stage)
 
-summary(lm(trend~TMean,data=subset(trendEstimates,Stage=="adult"),
+summary(lm(trend~TMean,data=trendEstimates,
                                    weights=1/trend_sd))#sig, temp mean higher tends
 #positive effect
 
@@ -29,15 +34,15 @@ ggplot(trendEstimates,aes(x=Habitat.y,y=trend))+
   geom_boxplot()+
   facet_wrap(State~Stage)
 
-summary(lm(trend~Habitat.y,data=subset(trendEstimates,Stage=="adult"),
-           weights=1/trend_sd))#sig - running, higher trends
+summary(lm(trend~Habitat.y,data=trendEstimates,
+           weights=1/trend_sd))#non sig - running, higher trends
 
 #range size
 ggplot(trendEstimates,aes(x=nuEuroGrids,y=trend))+
   geom_point()+
   facet_wrap(State~Stage)
 
-summary(lm(trend~nuEuroGrids,data=subset(trendEstimates,Stage=="adult"),
+summary(lm(trend~nuEuroGrids,data=trendEstimates,
            weights=1/trend_sd))#ns
 
 #generation time
@@ -45,13 +50,14 @@ ggplot(trendEstimates,aes(x=development.time..mean.,y=trend))+
   geom_point()+
   facet_wrap(State~Stage)
 
-summary(lm(trend~development.time..mean.,data=subset(trendEstimates,Stage=="adult"),
+summary(lm(trend~development.time..mean.,data=trendEstimates,
            weights=1/trend_sd))#marginal
 
 
 ####################################################################
 
 #plot trait vs trends
+
 q1<-ggplot(subset(trendEstimates,Stage=="adult"),aes(x=TMean,y=trend,colour=State))+
   geom_point()+
   theme_bw()+
@@ -96,6 +102,27 @@ plot_grid(q1,q2,align="v",ncol=1)
 
 library(gridExtra)
 grid.arrange(q5,q6,ncol=1)
+
+###################################################################
+
+#temp and habitat preference
+q1<-ggplot(trendEstimates,aes(x=TMean,y=trend,colour=State))+
+  geom_point()+
+  theme_bw()+
+  geom_hline(yintercept=0,colour="black",linetype="dashed")+
+  ylab("Population trend")+xlab("Temperature preference")+
+  stat_smooth(method="lm",se=F)+
+  theme(legend.position="none")
+
+q2<-ggplot(trendEstimates,aes(x=Habitat.y,y=trend,fill=State))+
+  geom_boxplot(outlier.shape = NA)+
+  theme_bw()+
+  geom_hline(yintercept=0,colour="black",linetype="dashed")+
+  ylab("Population trend")+xlab("Habitat preference")+
+  theme(legend.position="none")
+
+library(cowplot)
+plot_grid(q1,q2,align="h",ncol=2)
 
 #####################################################################
 
