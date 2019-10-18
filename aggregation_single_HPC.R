f7888620452a796d4d8785d03e9c1175fecae812
Bowler <db40fysa@idiv.de>
2019-10-18 11:50:15 +0200

    changing folder directory

--- aggregation_single_HPC.R
+++ aggregation_single_HPC.R
@@ -1,5 +1,5 @@
 #get species for this task
-myfolder <- "Odonata_adult_BB"
+myfolder <- "Odonata_adult_nation_state"
 
 #open folder and get list of files within
 folder1 <- paste0("/work/bowler/",myfolder)
@@ -11,11 +11,11 @@ library(plyr)
 modelSummary <- ldply(modelFiles, function(x){
   temp <- readRDS(paste(folder1,folder2,x,sep="/"))
   #get summary of parameters
-  tempSummary <- data.frame(temp$summary)
+  tempSummary <- data.frame(temp)
   tempSummary$Param <- as.character(row.names(tempSummary))
   tempSummary$File <- x
   return(tempSummary)
 })
 
 #save the folder name as a column
-saveRDS(modelSummary,file=paste0("modelSummary_flightperiod_",myfolder,".rds"))
+saveRDS(modelSummary,file=paste0("modelSummary_",myfolder,".rds"))
