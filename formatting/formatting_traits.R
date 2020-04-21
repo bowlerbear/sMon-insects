cf2392af1d497fc80b06810809fce23211916f47
Bowler <db40fysa@idiv.de>
2020-04-21 18:59:19 +0200

    include red list

--- formatting/formatting_traits.R
+++ formatting/formatting_traits.R
@@ -4,18 +4,21 @@ library(ggplot2)
 
 ###########################################################################################
 
+#define trait folder
+tdir <- "C:/Users/db40fysa/Nextcloud/sMon-Analyses/Insects_general/Odonata/traits"
+
 #get total specieslist
-species <- read.delim("C:/Users/db40fysa/Nextcloud/sMon-Analyses/Insects/traits/specieslist_odonata.txt",as.is=T)
+species <- read.delim(paste(tdir,"specieslist_odonata.txt",sep="/"),as.is=T)
 
 ###########################################################################################
 
 #get new atlas traits data data
-traits1<- read.csv("C:/Users/db40fysa/Nextcloud/sMon-Analyses/Insects/traits/10750_2017_3495_MOESM1_ESM.csv",as.is=T)
+traits1<- read.csv(paste(tdir,"10750_2017_3495_MOESM1_ESM.csv",sep="/"),as.is=T)
 traits1$species <- traits1$Species
 traits1$Species <- paste(traits1$Genus,traits1$Species)
 
 #get new atlas distribution data
-atlas <- read.csv("C:/Users/db40fysa/Nextcloud/sMon-Analyses/Insects/traits/10750_2017_3495_MOESM2_ESM.csv",as.is=T)
+atlas <- read.csv(paste(tdir,"10750_2017_3495_MOESM2_ESM.csv",sep="/"),as.is=T)
 #make sure first letter is a capital
 firstup <- function(x) {
   substr(x, 1, 1) <- toupper(substr(x, 1, 1))
@@ -36,34 +39,25 @@ traits1$Species[!traits1$Species %in% atlas$Species.name]
 #all there
 atlas$Species.name[!atlas$Species.name %in% traits1$Species]
 traits1 <- merge(traits1,atlas,by.x="Species",by.y="Species.name")
-all(species$Species %in% traits1$Species) 
+all(species$Species %in% traits1$Species) #all there = TRUE
 
 #get my trait compilation based off the atlas species list
-traits2 <- read.delim("C:/Users/db40fysa/Nextcloud/sMon-Analyses/Insects/traits/specieslist_Germany_atlastraits.txt",as.is=T)
+traits2 <- read.delim(paste(tdir,"specieslist_Germany_atlastraits.txt",sep="/"),as.is=T)
+
+#sort to columns we are interested in
+names(traits2)
+traits2 <- traits2[,c("Species","German_name","Family","Suborder","total","since",
+                      "Flight_start","Flight_end")]
+
 traits2 <- merge(traits2,traits1,by="Species")
 nrow(traits2)#should be 81
 head(traits2)
-
-# names(traits2)
-# [1] "Species"                             "German_name"                        
-# [3] "Family"                              "Suborder"                           
-# [5] "total"                               "since"                              
-# [7] "Europe_trend"                        "Flight_start"                       
-# [9] "Flight_end"                          "Water"                              
-# [11] "Habitat.x"                           "Comment"                            
-# [13] "Taxon"                               "Genus"                              
-# [15] "Habitat.y"                           "Endemic.to.Europe"                  
-# [17] "EU.Red.List.Europe"                  "Habitat.Directive"                  
-# [19] "Threath.status.on.European.Red.List" "species"                            
-# [21] "lowerLat"                            "upperLat"                           
-# [23] "minLong"                             "maxLong"                            
-# [25] "nuEuroGrids"
 
 #####################################################################################
 
 #get oliver schweigers - development tinme, voltinism, migration
 
-traits3 <- read.csv("C:/Users/db40fysa/Nextcloud/sMon-Analyses/Insects/traits/from Oliver Schweiger/dragonflies_STI_larval development time_withNotes.csv",as.is=T)
+traits3 <- read.csv(paste(tdir,"from Oliver Schweiger/dragonflies_STI_larval development time_withNotes.csv",sep="/"),as.is=T)
 traits3$species %in% species$Species
 traits3$species[!traits3$species %in% species$Species]
 traits3$species[which(traits3$species=="Lestes viridis")]<-"Chalcolestes viridis"
@@ -124,16 +118,16 @@ traits2 <- merge(traits2,traits3,by.x="Species",by.y="species",all.x=T)
 
 #get new temp preference data based on new atlas
 
-tempPref <- read.delim("C:/Users/db40fysa/Nextcloud/sMon-Analyses/Insects/traits/speciesNiches_germanDragonflies.txt")
+tempPref <- read.delim(paste(tdir,"speciesNiches_germanDragonflies.txt",sep="/"),as.is=T)
 all(tempPref$Species.name %in% traits2$Species) 
-
+#should be TRUE
 alltraits <- merge(traits2,tempPref,by.x="Species",by.y="Species.name",all=T)
 
 #####################################################################################
 
 #get new body size and wing length information
 
-bodysize <- read.csv("C:/Users/db40fysa/Nextcloud/sMon-Analyses/Insects/traits/sizes/specieslist_odonata_size.csv")
+bodysize <- read.csv(paste(tdir,"sizes/specieslist_odonata_size.csv",sep="/"),as.is=T)
 bodysize$medAb <- (bodysize$Ab.lower.in.mm + bodysize$Ab.upper.in.mm)/2
 bodysize$medHw <- (bodysize$Hindwing.lower.in.mm + bodysize$Hindwing.upper.in.mm)/2
 species$Species [!species$Species %in% bodysize$Species]
@@ -141,21 +135,42 @@ species$Species [!species$Species %in% bodysize$Species]
 #add to my data
 alltraits$medAb <- bodysize$medAb[match(alltraits$Species,bodysize$Species)] 
 alltraits$medHw <- bodysize$medHw[match(alltraits$Species,bodysize$Species)] 
-alltraits$HabitatBreadth <- bodysize$HabitatSpecialist[match(alltraits$Species,bodysize$Species)]
-alltraits$Flow <- bodysize$Flow[match(alltraits$Species,bodysize$Species)]
+
+######################################################################################
+
+#get my compiled habitat data
+habitat <- read.csv(paste(tdir,"habitat/Odonata_habitat_final.csv",sep="/"),as.is=T)
+habitat[is.na(habitat)] <- 0
+
+#select columns we will use
+habitat <- habitat[,1:18]
+alltraits$Species[!alltraits$Species %in% habitat$Species]#all there
+
+#Sort flow categories
+unique(habitat$Flow)
+table(habitat$Flow)
+habitat$Flow[which(habitat$Flow=="running (standing)")] <- "running"
+habitat$Flow[which(habitat$Flow=="slow-flowing")] <- "running"
+
+alltraits <- merge(alltraits,habitat,by="Species")
 
 ######################################################################################
 
 #traits from Frank Suhling:
 library(gdata)
-setwd("C:/Users/db40fysa/Nextcloud/sMon-Analyses/Insects/traits/from_Frank_Suhling")
-ftraits <- read.xls("Traits_German_Odonata.xls",skip=2)
+ftraits <- read.xls(paste(tdir,"from_Frank_Suhling/Traits_German_Odonata.xls",sep="/"),skip=2,as.is=T)
 ftraits <- ftraits[,c(4,10:21)]
 ftraits <- ftraits[,c(1:6,10:13)]
 names(ftraits)[1] <- "Species"
+
+#check names
 species$Species [!species$Species %in% ftraits$Species]
 #"Gomphus flavipes"   "Libellula depressa" "Libellula fulva"
-#check for synonyms
+ftraits$Species[which(ftraits$Species=="Stylurus flavipes")] <- "Gomphus flavipes"
+ftraits$Species[which(ftraits$Species=="Ladona depressa")] <- "Libellula depressa"
+ftraits$Species[which(ftraits$Species=="Ladona fulva")] <- "Libellula fulva"
+species$Species [!species$Species %in% ftraits$Species]
+#all there now
 
 #weighted mean of voltinism
 ftraits$VoltinismProp <- apply(ftraits,1,function(x)(t(as.numeric(x[2:6])) %*% 5:1)/10)
@@ -180,46 +195,96 @@ alltraits <- merge(alltraits,ftraits[,c("Species","VoltinismProp","Overwintering
 ######################################################################################
 
 #data from freshwater ecology
-fwecol <- read.csv("C:/Users/db40fysa/Nextcloud/sMon-Analyses/Insects/traits/from FWecolol/tvtexport_data.csv")
-#species name conversion
-fwecolSpecies<- read.csv("C:/Users/db40fysa/Nextcloud/sMon-Analyses/Insects/traits/from FWecolol/specieslist_odonata.csv")
-fwecol$Species <- fwecolSpecies$Species[match(fwecol$Fwecol_Species,fwecolSpecies$Fwecol_Species)]
+# fwecol <- read.csv("C:/Users/db40fysa/Nextcloud/sMon-Analyses/Insects/traits/from FWecolol/tvtexport_data.csv")
+# #species name conversion
+# fwecolSpecies<- read.csv("C:/Users/db40fysa/Nextcloud/sMon-Analyses/Insects/traits/from FWecolol/specieslist_odonata.csv")
+# fwecol$Species <- fwecolSpecies$Species[match(fwecol$Fwecol_Species,fwecolSpecies$Fwecol_Species)]
+# 
+# #get mean szp for each species
+# szp <- fwecol[,grepl("szp",names(fwecol))]
+# szp[is.na(szp)] <- 0
+# SZP <- apply(szp,1,function(x)(as.numeric(x) %*% 1:9)/sum(x))
+# SZP[is.na(SZP)]<- NA
+# 
+# #get mean saprotic index
+# saprobit_Austria <- fwecol$saprobity...Austria_si
+# 
+# #also for the netherlands
+# 
+# #xeno - non existent
+# #oligo - normal
+# #beta -moderate
+# #alpha - strong
+# #poly - present locally
+# saprobit_Netherlands <- fwecol[,grepl("saprobity...Netherlands",names(fwecol))]
+# saprobit_Netherlands[is.na(saprobit_Netherlands)] <- 0
+# saprobit_Netherlands <- apply(saprobit_Netherlands,1,function(x)(as.numeric(x) %*% 1:5)/sum(x))
+# saprobit_Netherlands[is.na(saprobit_Netherlands)]<- NA
+# 
+# #combine all
+# fwecol <- data.frame(Species=fwecol$Species,
+#                      SZP=SZP,
+#                      saprobit_Austria=saprobit_Austria,
+#                      saprobit_Netherlands=saprobit_Netherlands)
+# pairs(fwecol[,2:4])
+# #higher the zone preference, the higher the saprobit index
+# alltraits <- merge(alltraits,fwecol,by="Species")
 
-#get mean szp for each species
-szp <- fwecol[,grepl("szp",names(fwecol))]
-szp[is.na(szp)] <- 0
-SZP <- apply(szp,1,function(x)(as.numeric(x) %*% 1:9)/sum(x))
-SZP[is.na(SZP)]<- NA
+######################################################################################
 
-#get mean saprotic index
-saprobit_Austria <- fwecol$saprobity...Austria_si
+#get red list data
+redList <- read.csv("C:/Users/db40fysa/Nextcloud/sMon-Analyses/Insects_general/Odonata/traits/from_KJC/red list germany dragonflies 2015.csv",as.is=T)
 
-#also for the netherlands
+#sort species name out
+redList$Genus <- sapply(redList$Name,function(x)strsplit(x," ")[[1]][1])
+redList$species <- sapply(redList$Name,function(x)strsplit(x," ")[[1]][2])
+redList$Species <- paste(redList$Genus,redList$species)
+unique(redList$Species)
 
-#xeno - non existent
-#oligo - normal
-#beta -moderate
-#alpha - strong
-#poly - present locally
-saprobit_Netherlands <- fwecol[,grepl("saprobity...Netherlands",names(fwecol))]
-saprobit_Netherlands[is.na(saprobit_Netherlands)] <- 0
-saprobit_Netherlands <- apply(saprobit_Netherlands,1,function(x)(as.numeric(x) %*% 1:5)/sum(x))
-saprobit_Netherlands[is.na(saprobit_Netherlands)]<- NA
+#Check names against standard species list
+redList$Species[!redList$Species %in% species$Species]
+redList$Species[redList$Species %in% species$Species]
+#all names correct
 
-#combine all
-fwecol <- data.frame(Species=fwecol$Species,
-                     SZP=SZP,
-                     saprobit_Austria=saprobit_Austria,
-                     saprobit_Netherlands=saprobit_Netherlands)
-pairs(fwecol[,2:4])
-#higher the zone preference, the higher the saprobit index
-alltraits <- merge(alltraits,fwecol,by="Species")
+#covert categories into something more meaningful
+unique(redList$RL.Kat.)
+"*" #ungafahrdet #not in danger
+"1" #vom Aussterben bedroht #threatened with extinction
+"V" #Vorwarnliste #early warning list
+"2" #Stak gefahrdet #strongly in danger
+"R" #Extrem selten #extremely rare
+"0" #Ausgestorben oder verschollen #extinct or lost # "Coenagrion hylas"      "Onychogomphus uncatus"
+"3" # Gefahrhdet # endanger
+
+#see Jansen et al. translation
+redList$RedListCat <- NA
+redList$RedListCat[redList$RL.Kat.=="0"] <- "regionally extinct"
+redList$RedListCat[redList$RL.Kat.=="1"] <- "critically endangered"
+redList$RedListCat[redList$RL.Kat.=="2"] <- "endangered"
+redList$RedListCat[redList$RL.Kat.=="3"] <- "vulnerable"
+redList$RedListCat[redList$RL.Kat.=="R"] <- "vulnerable"
+redList$RedListCat[redList$RL.Kat.=="V"] <- "near threatened"
+redList$RedListCat[redList$RL.Kat.=="*"] <- "least concern"
+table(redList$RedListCat)
+
+#merge with species list
+species$RedListCat <- redList$RedListCat[match(species$Species,redList$Species)]
+species$GermanRedList <- redList$RL.Kat.[match(species$Species,redList$Species)]
+subset(species,is.na(RedListCat))
+#Anax ephippiger
+#Lestes macrostigma
+species$RedListCat[is.na(species$RedListCat)] <- "not listed"
+write.table(species,file="C:/Users/db40fysa/Nextcloud/sMon-Analyses/Insects_general/Odonata/traits/from_KJC/redlist_formatted.txt",row.names=FALSE,sep="\t")
+
+#add to all traits
+alltraits$Species[!alltraits$Species %in% species$Species]
+alltraits$RedList <- species$RedListCat[match(alltraits$Species,species$Species)]
+alltraits$GermanRedList <- species$GermanRedList[match(alltraits$Species,species$Species)]
 
 #####################################################################################
 
 #rename some column headings
 names(alltraits)[which(names(alltraits)=="total")] <- "germanRange"
-names(alltraits)[which(names(alltraits)=="Habitat.y")] <- "Habitat"
 names(alltraits)[which(names(alltraits)=="development.time..mean.")] <- "devTime"
 
 ######################################################################################
