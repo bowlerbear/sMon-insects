faf993a9b0514edb958a97b654722c63ab4098ae
Diana Bowler <diana.bowler@idiv.de>
2021-01-04 11:04:23 +0100

    examine midnaturraum trends map

--- modelSummaries_regional.R
+++ modelSummaries_regional.R
@@ -19,13 +19,13 @@ plotTS_regional <- function(x){
   print(g1)
 }
 
-###Species list#################################################################################################
+###Species list#############################################
 
 mySpecies <- read.delim("model-auxfiles/speciesTaskID_adult.txt",as.is=T)$Species
 
 allSpecies <- read.delim("specieslist_odonata.txt",as.is=T)$Species
 
-### get ecoregions #########################################################################################
+### get ecoregions ########################################
 
 nr <- st_read("C:/Users/db40fysa/Nextcloud/sMon/sMon-Analyses/Spatial_data/Narurraeume/naturraeume_polygon.shp")
 
@@ -50,9 +50,9 @@ CoarseRegions <- c("Alpen","Alpenvorland","Nordostdeutsches Tiefland","Nordwestd
                    "Oestliches Mittelgebirge","Suedwestdeutsches Mittelgebirge ",
                    "Westliches Mittelgebirge")
 
-###Model summaries#########################################################################################
+### model summaries########################################
 
-### ecoregion 1 ############################################################################################
+### ecoregion 1 (coarse) ##################################
 
 mdir <- "C:/Users/db40fysa/Nextcloud/sMon/sMon-Analyses/Odonata_Git/sMon-insects/model-outputs/Odonata_sparta_regional/6869992"
 
@@ -141,7 +141,7 @@ nr_dissolved  <- left_join(nr_dissolved,ecoregionMeans,by="CoarseNaturraum")
 tm_shape(nr_dissolved)+
   tm_polygons("spoccChange")
 
-#### ecoregion 2 model ###################################################################
+#### ecoregion 3 (finest) #################################
 
 mdir <- "C:/Users/db40fysa/Nextcloud/sMon/sMon-Analyses/Odonata_Git/sMon-insects/model-outputs/Odonata_adult_regional_nation_raumunit_sparta/7180407"
 
@@ -187,8 +187,57 @@ tm_shape(nr_sf)+
 
 #looks like very little change over time...
 
-### spline model #########################################################################
+### ecoregion 2 (mid) ##################################
+
+mdir <- "C:/Users/db40fysa/Nextcloud/sMon/sMon-Analyses/Odonata_Git/sMon-insects/model-outputs/Odonata_adult_regional_midnaturraumtrend_sparta/7474533"
+
+#do we have the models for all species?
+speciesFiles <- list.files(mdir)
+mySpecies[!sapply(mySpecies,function(x)any(grepl(x,speciesFiles)))]
+
+#read in model summaries
+modelDF <- getModelSummaries(mdir)
+modelDF <- getCodeFromFile(modelDF,myfile="out_sparta_regional_nation_midnaturraumtrends_adult_")
+
+#what parameters do we have?
+unique(modelDF$Param)
+
+#annual time series - psi.fs
+annualtrendsDF <- getBUGSfits(modelDF,param="mraumtrend")
+
+#find out which indices correspond to which
+siteInfo <- readRDS("siteInfo_midnaturraum.rds")
+annualtrendsDF$MidNaturraum <- siteInfo$MidNaturraum[
+  match(annualtrendsDF$ParamNu,siteInfo$mnIndex)]
+
+#get % of increasing species per naturraum
+increasingProp <- annualtrendsDF %>%
+                    group_by(MidNaturraum) %>%
+                    summarise(incr_prop = mean(mean>0))
+
+#add also to the nr shapefile
+load("mtbqsDF.RData")
+nr$MidNaturraum <- gdata::trim(mtbqsDF$MTB_MidNatur[match(nr$Name,mtbqsDF$MTB_Natur)])
+
+#dissolve to midnaturraum
+nr_dissolved <-
+  nr %>%
+  group_by(MidNaturraum) %>%
+  summarise()
+plot(nr_dissolved)
+
+#merge summary data and shapefile
+all(increasingProp$MidNaturraum %in% nr$MidNaturraum)#TRUE
+nr_dissolved  <- left_join(nr_dissolved,increasingProp,by="MidNaturraum")
+
+tm_shape(nr_dissolved)+
+  tm_polygons("incr_prop")
+
+#no obvious pattern
+#no data from some midnaturraums
+
+### spline model ##########################################
 
 
 
-###end####################################################################################
+###end######################################################
