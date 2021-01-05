c8b0db3cfdf801c86ea9831a9fc180ca6f11143a
Diana Bowler <diana.bowler@idiv.de>
2021-01-05 09:37:15 +0100

    examine the model ran on the MTBQ surveyed once per decade subset

--- sensitivityAnalysis.R
+++ sensitivityAnalysis.R
@@ -51,3 +51,38 @@ temp <- ddply(annualDFcomparison,.(Species),function(x){
   cor(x$mean.x,x$mean.y)
 })
 median(temp$V1)
+
+
+### mtbq subset ########################################
+
+mdir <- "C:/Users/db40fysa/Nextcloud/sMon/sMon-Analyses/Odonata_Git/sMon-insects/model-outputs/Odonata_adult_nation_naturraum_sparta_MTBQ_eachDecade/7503038"
+
+#do we have the models for all species?
+speciesFiles <- list.files(mdir)
+mySpecies[!sapply(mySpecies,function(x)any(grepl(x,speciesFiles)))]
+#"Crocothemis erythraea" "Oxygastra curtisii"
+
+#read in model summaries
+modelDF <- getModelSummaries(mdir)
+modelDF <- getCodeFromFile(modelDF,
+                           myfile="out_sparta_nation_naturraum_MTBQ_eachDecadeadult_")
+
+#annual time series in occupancy
+annualDFs <- getBUGSfits(modelDF,param="psi.fs")
+annualDFs$Year <- annualDFs$ParamNu + 1979
+plotTS(annualDFs)
+table(annualDFs$Rhat<1.1)
+summary(annualDFs$Rhat[annualDF$Rhat>1.1])
+
+#trends in occupancy
+trendsDFs <- getBUGSfits(modelDF,param="regres.psi")
+
+### comparison #####################################
+
+trendsDFcomparison <- merge(trendsDF,trendsDFs,by="Species")
+qplot(mean.x,mean.y,data=trendsDFcomparison)
+cor.test(trendsDFcomparison$mean.x,trendsDFcomparison$mean.y)
+cor.test(trendsDFcomparison$sd.x,trendsDFcomparison$sd.y)
+
+qplot(sd.x,sd.y,data=trendsDFcomparison)
+qplot(mean.x,mean.y,data=trendsDFcomparison)
