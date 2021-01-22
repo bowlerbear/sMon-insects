ceb9d6e20009842c3b9aa7e567eff8781c8cfc66
Diana Bowler <diana.bowler@idiv.de>
2021-01-22 13:25:38 +0100

    examine model with more terms on the detection model

--- sensitivityAnalysis.R
+++ sensitivityAnalysis.R
@@ -106,7 +106,7 @@ annualDFs <- getBUGSfits(modelDF,param="psi.fs")
 annualDFs$Year <- annualDFs$ParamNu + 1979
 plotTS(annualDFs)
 table(annualDFs$Rhat<1.1)
-summary(annualDFs$Rhat[annualDF$Rhat>1.1])
+summary(annualDFs$Rhat[annualDFs$Rhat>1.1])
 
 #trends in occupancy
 trendsDFs <- getBUGSfits(modelDF,param="regres.psi")
@@ -120,5 +120,108 @@ cor.test(trendsDFcomparison$sd.x,trendsDFcomparison$sd.y)
 
 qplot(sd.x,sd.y,data=trendsDFcomparison)
 qplot(mean.x,mean.y,data=trendsDFcomparison)
+
+
+### detection model #################################
+
+#including coarse and mid naturruam on detection probability
+mdir <- "C:/Users/db40fysa/Nextcloud/sMon/sMon-Analyses/Odonata_Git/sMon-insects/model-outputs/Odonata_adult_nation_naturraum_detModel/7564941"
+
+#do we have the models for all species?
+speciesFiles <- list.files(mdir)
+mySpecies[!sapply(mySpecies,function(x)any(grepl(x,speciesFiles)))]
+#"Erythromma lindenii"  "Ischnura elegans"     "Lestes sponsa"        "Orthetrum albistylum" "Sympecma paedisca" [6] "Sympetrum striolatum"
+
+#read in model summaries
+modelDF <- getModelSummaries(mdir)
+modelDF <- getCodeFromFile(modelDF,
+                           myfile="out_sparta_nation_naturraum_detModel_adult_")
+
+#annual time series in occupancy
+annualDFs <- getBUGSfits(modelDF,param="psi.fs")
+annualDFs$Year <- annualDFs$ParamNu + 1979
+plotTS(annualDFs)
+table(annualDFs$Rhat<1.1)
+summary(annualDFs$Rhat[annualDFs$Rhat>1.1])
+
+#trends in occupancy
+trendsDFs <- getBUGSfits(modelDF,param="regres.psi")
+table(trendsDFs$Rhat<1.1)
+#FALSE  TRUE 
+#9    62
+
+#annual detection probabilities
+annualDF <- getBUGSfits(modelDF,param="annual.p")
+annualDF$Year <- annualDF$ParamNu + 1979
+plotDetections(annualDF)
+table(annualDF$Rhat<1.1)
+annualDF$Species[annualDF$Rhat>1.1]
+#worse convergence
+
+#bpv
+bpvDF <- getBUGSfits(modelDF,param="bpv")
+summary(bpvDF$mean)
+#better...
+
+### comparison ####################################
+
+trendsDFcomparison <- merge(trendsDF,trendsDFs,by="Species")
+qplot(mean.x,mean.y,data=trendsDFcomparison)
+cor.test(trendsDFcomparison$mean.x,trendsDFcomparison$mean.y)
+cor.test(trendsDFcomparison$sd.x,trendsDFcomparison$sd.y)
+
+qplot(sd.x,sd.y,data=trendsDFcomparison)
+qplot(mean.x,mean.y,data=trendsDFcomparison)
+
+### excluding naturalist data #######################
+
+#including coarse and mid naturruam on detection probability
+mdir <- "C:/Users/db40fysa/Nextcloud/sMon/sMon-Analyses/Odonata_Git/sMon-insects/model-outputs/Odonata_adult_nation_naturraum_statesOnly/7563237"
+
+#do we have the models for all species?
+speciesFiles <- list.files(mdir)
+mySpecies[!sapply(mySpecies,function(x)any(grepl(x,speciesFiles)))]
+#"Nehalennia speciosa"    "Somatochlora alpestris" "Sympetrum meridionale"  "Sympetrum sanguineum"   "Sympetrum striolatum"
+
+#read in model summaries
+modelDF <- getModelSummaries(mdir)
+modelDF <- getCodeFromFile(modelDF,
+                           myfile="out_sparta_nation_naturraum_statesOnly_adult_")
+
+#annual time series in occupancy
+annualDFs <- getBUGSfits(modelDF,param="psi.fs")
+annualDFs$Year <- annualDFs$ParamNu + 1979
+plotTS(annualDFs)
+table(annualDFs$Rhat<1.1)
+summary(annualDFs$Rhat[annualDFs$Rhat>1.1])
+
+#trends in occupancy
+trendsDFs <- getBUGSfits(modelDF,param="regres.psi")
+table(trendsDFs$Rhat<1.1)
+#FALSE  TRUE 
+#3    69
+
+#annual detection probabilities
+annualDF <- getBUGSfits(modelDF,param="annual.p")
+annualDF$Year <- annualDF$ParamNu + 1979
+plotDetections(annualDF)
+table(annualDF$Rhat<1.1)
+annualDF$Species[annualDF$Rhat>1.1]
+#worse convergence
+
+#bpv
+bpvDF <- getBUGSfits(modelDF,param="bpv")
+summary(bpvDF$mean)
+
+### comparison ####################################
+
+trendsDFcomparison <- merge(trendsDF,trendsDFs,by="Species")
+cor.test(trendsDFcomparison$mean.x,trendsDFcomparison$mean.y)
+cor.test(trendsDFcomparison$sd.x,trendsDFcomparison$sd.y)
+
+qplot(sd.x,sd.y,data=trendsDFcomparison)
+qplot(mean.x,mean.y,data=trendsDFcomparison)
+
+#almost the same
 
 ### end ###########################################
